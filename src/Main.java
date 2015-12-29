497922b036ef6b84966380f220a4d16b50edaf6e
rych <ryskuduk@gmail.com>
2015-12-29 18:31:53 +0100

    start

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by RYchu on 2015-12-18.
+ */
+
+public class Main {
+
+    private int numer;
+    public int wiek;
+    protected int kek;
+    public static void wypisz(){
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args){
+        wypisz();
+    }
+}
---
+++ src/Swiat.java
@@ -0,0 +1,5 @@
+/**
+ * Created by RYchu on 2015-12-18.
+ */
+public class Swiat {
+}
