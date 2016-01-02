67b7c96af599053b6915a46e2f89b73037c54964
rych <ryskuduk@gmail.com>
2016-01-02 18:38:47 +0100

    wsadzZwierzakaDoSwiata

--- src/Swiat.java
+++ src/Swiat.java
@@ -2,6 +2,23 @@
  * Created by RYchu on 2015-12-18.
  */
 public class Swiat {
+    public Mapa m;
 
+    public void wsadzZwierzakaDoSwiata(int x, int y, char zwierzakAscii) {
+        Organizm organizmWsadzany;
 
+        switch (zwierzakAscii) {
+            case 'L': {
+                organizmWsadzany = new Lew();
+                break;
+            }
+            default:{
+                organizmWsadzany = null;
+            }
+        }
+        //kolejka.addNode(organizmWsadzany);
+        m.organizmyTab[x][y] = organizmWsadzany;
+        if(organizmWsadzany!=null)
+            organizmWsadzany.przypiszXY(x, y);
+    }
 }
